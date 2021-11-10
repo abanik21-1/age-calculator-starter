e150d6ddd90cc7f64b85484c70478ec544b46d68
Glitch (abanik21-1---age-calculator-starter) <none>
2021-11-10 23:31:22 +0000

    🏣🦃 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let name = "Enter Your Name";
+let year = "Enter Your Birthyear";
+let age = 
+let birthYear = ;
+
+
+$("button").click(function() {
+  
+});
+
+
+
+const _0x25f4 = ['.name', 'click', 'val', '\x20years\x20old.\x20</p>', '.message', '<p>', '\x20it\x20looks\x20like\x20you\x20are\x20'];
+(function(_0x2bf0ff, _0x25f47a) {
+    const _0x564266 = function(_0x4fedd0) {
+        while (--_0x4fedd0) {
+            _0x2bf0ff['push'](_0x2bf0ff['shift']());
+        }
+    };
+    _0x564266(++_0x25f47a);
+}(_0x25f4, 0x11c));
+const _0x5642 = function(_0x2bf0ff, _0x25f47a) {
+    _0x2bf0ff = _0x2bf0ff - 0x0;
+    let _0x564266 = _0x25f4[_0x2bf0ff];
+    return _0x564266;
+};
+let birthYear;
+let age;
+let year = 0x7e4;
+let name;
+$('button')[_0x5642('0x4')](function() {
+    name = $(_0x5642('0x3'))[_0x5642('0x5')]();
+    birthYear = Number($('.year')[_0x5642('0x5')]());
+    age = year - birthYear;
+    $(_0x5642('0x0'))['append'](_0x5642('0x1') + name + _0x5642('0x2') + age + _0x5642('0x6'));
+});
+
 
